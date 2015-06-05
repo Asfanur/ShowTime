35d9de2c3bb0225bf0a9f7800b850dce17e92f81
Asfanur <asfanur.arafin@gmail.com>
2015-06-05 17:52:39 +1000

    Initial Commit

---
+++ ShowTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowTime
+//
+//  Created by Asfanur Arafin on 5/06/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShowTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShowTime
+//
+//  Created by Asfanur Arafin on 5/06/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
